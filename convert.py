bfc31a4612da0615630182a523f80b43f2d1a9fe
Toru Ogawa <Hakuyume@users.noreply.github.com>
2018-07-04 20:00:48 +0900

    use Conv

--- convert.py
+++ convert.py
@@ -3,18 +3,24 @@ import numpy as np
 from unittest import mock
 
 import chainer
-import chainer.functions as F
+import chainer.links as L
 import chainercv
 import onnx_chainer
 
 
-# FIXME
-def reorg(x):
-    n, c, h, w = x.shape
-    return F.reshape(x, (n, c * 4, h // 2, w // 2))
+# YOLOv2 with some hacks
+class YOLOv2(chainercv.links.YOLOv2):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        with self.init_scope():
+            self.reorg = L.Convolution2D(64, 256, 2, stride=2, nobias=True)
+        self.reorg.W.array[:] = 0
 
-chainercv.links.model.yolo.yolo_v2._reorg = reorg
+    def __call__(self, x):
+        with mock.patch(
+                'chainercv.links.model.yolo.yolo_v2._reorg', self.reorg):
+            return self.subnet(self.extractor(x))
 
 
 class IDGenerator(object):
@@ -38,14 +44,11 @@ def main():
     parser.add_argument('--out', default='YOLOv2.onnx')
     args = parser.parse_args()
 
-    model = chainercv.links.YOLOv2(pretrained_model='voc0712')
-
+    model = YOLOv2(pretrained_model='voc0712')
     x = np.empty((1, 3, model.insize, model.insize), dtype=np.float32)
     with chainer.using_config('train', False), \
             mock.patch('builtins.id', IDGenerator()):
-        model = onnx_chainer.export(
-            chainer.Sequential(model.extractor, model.subnet),
-            x, filename=args.out)
+        onnx_chainer.export(model, x, filename=args.out)
 
 
 if __name__ == '__main__':
